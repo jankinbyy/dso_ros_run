8703d919de286e3b8f20c88ff6d802daa2996571
jankinbyy <bai.yangyang@outlook.com>
2024-08-28 14:44:33 +0800

    euroc

--- run_ros.sh
+++ run_ros.sh
@@ -1,4 +1,4 @@
-build/bin/dso_ros image:=/camera/rgb/image_raw \
-		calib=/home/jankin/linux/slam/dso/config/camera.txt \
-		gamma=/home/jankin/linux/slam/dso/config/pcalib.txt \
-		vignette=/home/jankin/linux/slam/dso/config/config/vignette.png
+build/bin/dso_ros image:=/cam0/image_raw \
+		calib=/home/jankin/linux/slam/dso/config/euroc/camera.txt \
+		gamma=/home/jankin/linux/slam/dso/config/euroc/pcalib.txt \
+		vignette=/home/jankin/linux/slam/dso/config/config/euroc/vignette.png
